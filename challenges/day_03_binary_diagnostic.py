f672e5e7b25f7ad907b10d57a66c35f710524815
rebeckao <rebecka.oefverholm@gmail.com>
2021-12-05 13:51:14 +0100

    Day 3 part 2

--- challenges/day_03_binary_diagnostic.py
+++ challenges/day_03_binary_diagnostic.py
@@ -2,15 +2,50 @@ from typing import List, Tuple
 
 
 def power_consumption(diagnostics: List[str]) -> Tuple[int, int]:
-    gamma_rate_digits = [0 for _ in diagnostics[0]]
+    common_digits = [0 for _ in diagnostics[0]]
     for measurement in diagnostics:
         for idx, digit in enumerate(measurement):
             if measurement[idx] == '1':
-                gamma_rate_digits[idx] += 1
+                common_digits[idx] += 1
             else:
-                gamma_rate_digits[idx] -= 1
-    binary_gamma = [1 if digit > 0 else 0 for digit in gamma_rate_digits]
-    gamma_rate = sum([value * pow(2, (len(binary_gamma) - idx - 1)) for idx, value in enumerate(binary_gamma)])
-    binary_epsilon = [0 if digit > 0 else 1 for digit in gamma_rate_digits]
-    epsilon_rate = sum([value * pow(2, (len(binary_epsilon) - idx - 1)) for idx, value in enumerate(binary_epsilon)])
+                common_digits[idx] -= 1
+    binary_gamma = [1 if digit > 0 else 0 for digit in common_digits]
+    gamma_rate = _binary_to_decimal(binary_gamma)
+    binary_epsilon = [0 if digit > 0 else 1 for digit in common_digits]
+    epsilon_rate = _binary_to_decimal(binary_epsilon)
     return gamma_rate, epsilon_rate
+
+
+def life_support_rating(diagnostics: List[str]) -> Tuple[int, int]:
+    oxygen_rate_binary = _resolve_rate(_most_common_digit_at_index, diagnostics)
+    co2_rate_binary = _resolve_rate(_least_common_digit_at_index, diagnostics)
+    return _binary_to_decimal(oxygen_rate_binary), _binary_to_decimal(co2_rate_binary)
+
+
+def _resolve_rate(bit_criteria, diagnostics):
+    candidates = diagnostics
+    index = 0
+    while len(candidates) > 1:
+        correct_digit = bit_criteria(candidates, index)
+        candidates = [candidate for candidate in candidates if int(candidate[index]) == correct_digit]
+        index += 1
+    return [1 if int(digit) > 0 else 0 for digit in candidates[0]]
+
+
+def _least_common_digit_at_index(measurements: List[str], index: int) -> int:
+    return 1 - _most_common_digit_at_index(measurements, index)
+
+
+def _most_common_digit_at_index(measurements: List[str], index: int) -> int:
+    common_digit = 0
+    for measurement in measurements:
+        if measurement[index] == '1':
+            common_digit += 1
+        else:
+            common_digit -= 1
+    return 1 if common_digit >= 0 else 0
+
+
+def _binary_to_decimal(binary_gamma):
+    gamma_rate = sum([value * pow(2, (len(binary_gamma) - idx - 1)) for idx, value in enumerate(binary_gamma)])
+    return gamma_rate
--- challenges/test_day_03_binary_diagnostic.py
+++ challenges/test_day_03_binary_diagnostic.py
@@ -1,6 +1,6 @@
 from unittest import TestCase
 
-from challenges.day_03_binary_diagnostic import power_consumption
+from challenges.day_03_binary_diagnostic import power_consumption, life_support_rating
 
 
 class Test(TestCase):
@@ -30,4 +30,32 @@ class Test(TestCase):
             gamma_rate, epsilon_rate = power_consumption(diagnostic)
             self.assertEqual(1300, gamma_rate)
             self.assertEqual(2795, epsilon_rate)
-            self.assertEqual(3633500, gamma_rate * epsilon_rate)  # 14536600 too high
+            self.assertEqual(3633500, gamma_rate * epsilon_rate)
+
+    def test_life_support_rating(self):
+        diagnostic = [
+            "00100",
+            "11110",
+            "10110",
+            "10111",
+            "10101",
+            "01111",
+            "00111",
+            "11100",
+            "10000",
+            "11001",
+            "00010",
+            "01010"
+        ]
+        oxygen_generator_rate, co2_scrubber_rate = life_support_rating(diagnostic)
+        self.assertEqual(23, oxygen_generator_rate)
+        self.assertEqual(10, co2_scrubber_rate)
+        self.assertEqual(230, oxygen_generator_rate * co2_scrubber_rate)
+
+    def test_life_support_rating_real_input(self):
+        with open('inputs/day_03.txt') as f:
+            diagnostic = f.read().splitlines()
+            gamma_rate, epsilon_rate = life_support_rating(diagnostic)
+            self.assertEqual(1327, gamma_rate)
+            self.assertEqual(3429, epsilon_rate)
+            self.assertEqual(4550283, gamma_rate * epsilon_rate)
