3e069c61c4a964dbe9caa4d17f7ad971d1ad5a0d
rebeckao <rebecka.oefverholm@gmail.com>
2021-12-10 21:22:08 +0100

    Day 8 part 2

--- challenges/day_08_seven_segment_search.py
+++ challenges/day_08_seven_segment_search.py
@@ -1,9 +1,67 @@
-from typing import List
+from typing import List, Dict
 
 
 def number_of_identifiable_digits_in_output(input: List[str]):
     number_of_identifiable_digits = 0
     for line in input:
         lengths = [len(digit) for digit in line.split("|")[1].split(" ")]
-        number_of_identifiable_digits += len([length for length in lengths if length in [2, 4, 3, 7]])
+        number_of_identifiable_digits += len([length for length in lengths if length in [2, 4, 3, 7]])  # 1,4,7,8
     return number_of_identifiable_digits
+
+
+def wire_mapping(digit_lines: str) -> Dict[str, int]:
+    digits = [digit for digit in digit_lines.split(" ") if digit != "|"]
+    ones = [digit for digit in digits if len(digit) == 2][0]
+    fours = [digit for digit in digits if len(digit) == 4][0]
+    sevens = [digit for digit in digits if len(digit) == 3][0]
+    twos_or_threes_or_fives = set([digit for digit in digits if len(digit) == 5])
+    zeros_or_sixes_or_nines = set([digit for digit in digits if len(digit) == 6])
+    sides = {}
+    for char in "abcdefg":
+        if char in sevens and char not in ones:
+            sides["top"] = char
+        elif char in fours and char_is_in_each(char, twos_or_threes_or_fives):
+            sides["middle"] = char
+        elif char_is_in_each(char, twos_or_threes_or_fives):
+            sides["bottom"] = char
+        elif char_is_in_each(char, zeros_or_sixes_or_nines):
+            if char in ones:
+                sides["bottom-right"] = char
+            else:
+                sides["top-left"] = char
+        elif char in ones:
+            sides["top-right"] = char
+        else:
+            sides["bottom-left"] = char
+    return {
+        as_sorted_string({sides["top"], sides["top-left"], sides["top-right"], sides["bottom-left"], sides["bottom-right"], sides["bottom"]}): 0,
+        as_sorted_string({sides["top-right"], sides["bottom-right"]}): 1,
+        as_sorted_string({sides["top"], sides["top-right"], sides["middle"], sides["bottom-left"], sides["bottom"]}): 2,
+        as_sorted_string({sides["top"], sides["top-right"], sides["middle"], sides["bottom-right"], sides["bottom"]}): 3,
+        as_sorted_string({sides["top-left"], sides["top-right"], sides["middle"], sides["bottom-right"]}): 4,
+        as_sorted_string({sides["top"], sides["top-left"], sides["middle"], sides["bottom-right"], sides["bottom"]}): 5,
+        as_sorted_string({sides["top"], sides["top-left"], sides["middle"], sides["bottom-left"], sides["bottom-right"], sides["bottom"]}): 6,
+        as_sorted_string({sides["top"], sides["top-right"], sides["bottom-right"]}): 7,
+        as_sorted_string({sides["top"], sides["top-left"], sides["top-right"], sides["middle"], sides["bottom-left"], sides["bottom-right"], sides["bottom"]}): 8,
+        as_sorted_string({sides["top"], sides["top-left"], sides["top-right"], sides["middle"], sides["bottom-right"], sides["bottom"]}): 9
+    }
+
+
+def digits_in_output(digit_line):
+    mapping = wire_mapping(digit_line)
+    output_digits = [digit for digit in digit_line.split("|")[1].split(" ") if len(digit) > 0]
+    identified_output_digits = [mapping[as_sorted_string(digit)] for digit in output_digits]
+    return int("".join([str(digit) for digit in identified_output_digits]))
+
+
+def char_is_in_each(char, digits):
+    for digit in digits:
+        if char not in digit:
+            return False
+    return True
+
+
+def as_sorted_string(as_set):
+    as_list = list(as_set)
+    as_list.sort()
+    return "".join(as_list)
--- challenges/test_day_08_seven_segment_search.py
+++ challenges/test_day_08_seven_segment_search.py
@@ -1,6 +1,7 @@
 from unittest import TestCase
 
-from challenges.day_08_seven_segment_search import number_of_identifiable_digits_in_output
+from challenges.day_08_seven_segment_search import number_of_identifiable_digits_in_output, wire_mapping, \
+    as_sorted_string, digits_in_output
 
 
 class Test(TestCase):
@@ -22,3 +23,40 @@ class Test(TestCase):
         with open('inputs/day_08.txt') as f:
             actual = number_of_identifiable_digits_in_output(f.read().splitlines())
             self.assertEqual(247, actual)
+
+    def test_wire_mapping(self):
+        actual = wire_mapping("acedgfb cdfbe gcdfa fbcad dab cefabd cdfgeb eafb cagedb ab | cdfeb fcadb cdfeb cdbaf")
+        expected = {
+            "abcdeg": 0,
+            "ab": 1,
+            "acdfg": 2,
+            "abcdf": 3,
+            "abef": 4,
+            "bcdef": 5,
+            "bcdefg": 6,
+            "abd": 7,
+            "abcdefg": 8,
+            "abcdef": 9
+        }
+        self.assertEqual(expected, actual)
+
+    def test_as_sorted_string(self):
+        self.assertEqual("abcdef", as_sorted_string("fedcba"))
+        self.assertEqual("abc", as_sorted_string({"b", "c", "a"}))
+
+    def test_digits_in_output(self):
+        self.assertEqual(digits_in_output("be cfbegad cbdgef fgaecd cgeb fdcge agebfd fecdb fabcd edb | fdgacbe cefdb cefbgd gcbe"), 8394)
+        self.assertEqual(digits_in_output("edbfga begcd cbg gc gcadebf fbgde acbgfd abcde gfcbed gfec | fcgedb cgb dgebacf gc"), 9781)
+        self.assertEqual(digits_in_output("fgaebd cg bdaec gdafb agbcfd gdcbef bgcad gfac gcb cdgabef | cg cg fdcagb cbg"), 1197)
+        self.assertEqual(digits_in_output("fbegcd cbd adcefb dageb afcb bc aefdc ecdab fgdeca fcdbega | efabcd cedba gadfec cb"), 9361)
+        self.assertEqual(digits_in_output("aecbfdg fbg gf bafeg dbefa fcge gcbea fcaegb dgceab fcbdga | gecf egdcabf bgf bfgea"), 4873)
+        self.assertEqual(digits_in_output("fgeab ca afcebg bdacfeg cfaedg gcfdb baec bfadeg bafgc acf | gebdcfa ecba ca fadegcb"), 8418)
+        self.assertEqual(digits_in_output("dbcfg fgd bdegcaf fgec aegbdf ecdfab fbedc dacgb gdcebf gf | cefg dcbef fcge gbcadfe"), 4548)
+        self.assertEqual(digits_in_output("bdfegc cbegaf gecbf dfcage bdacg ed bedf ced adcbefg gebcd | ed bcgafe cdgba cbgef"), 1625)
+        self.assertEqual(digits_in_output("egadfb cdbfeg cegd fecab cgb gbdefca cg fgcdab egfdb bfceg | gbdfcae bgc cg cgb"), 8717)
+        self.assertEqual(digits_in_output("gcafb gcf dcaebfg ecagb gf abcdeg gaef cafbge fdbac fegbdc | fgae cfgab fg bagce"), 4315)
+
+    def test_digits_in_output_real_input(self):
+        with open('inputs/day_08.txt') as f:
+            sum_of_outputs = sum([digits_in_output(digit_line) for digit_line in f.read().splitlines()])
+            self.assertEqual(933305, sum_of_outputs)
