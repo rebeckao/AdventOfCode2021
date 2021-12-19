fbb981b7d9a76d6476085c03ca0ea716ce291aff
rebeckao <rebecka.oefverholm@gmail.com>
2021-12-19 21:29:06 +0100

    Day 14 part 2

--- challenges/day_14_extended_polymerization.py
+++ challenges/day_14_extended_polymerization.py
@@ -2,12 +2,8 @@ from collections import Counter
 from typing import List
 
 
-def polymer_after_steps(template: str, substitutions: List[str], steps: int):
-    subs = dict()
-    for sub_raw in substitutions:
-        sub = sub_raw.split(" -> ")
-        subs[sub[0]] = sub[1]
-    polymer = template
+def polymer_after_steps(template, subs, steps):
+    polymer = list(template)
     for _ in range(0, steps):
         new_polymer = ""
         prev_char = ""
@@ -19,8 +15,62 @@ def polymer_after_steps(template: str, substitutions: List[str], steps: int):
     return polymer
 
 
+def parse_substitutions(substitutions):
+    subs = dict()
+    for sub_raw in substitutions:
+        sub = sub_raw.split(" -> ")
+        subs[sub[0]] = sub[1]
+    return subs
+
+
 def polymer_hash(template: str, substitutions: List[str], steps: int):
-    polymer = polymer_after_steps(template, substitutions, steps)
+    polymer = polymer_after_steps(template, parse_substitutions(substitutions), steps)
     counter = Counter(polymer)
     sorted_elements = sorted(counter.items(), key=lambda item: item[1])
+    return sorted_elements[-1][1] - sorted_elements[0][1]
+
+
+def parse_template(template):
+    polymer = dict()
+    for idx, char in enumerate(template[0:-1]):
+        pair = char + template[idx + 1]
+        occurrences_of_pair = polymer.get(pair, 0)
+        polymer[pair] = occurrences_of_pair + 1
+    return polymer
+
+
+def increase_by(multiplier, the_dict, key):
+    prev = the_dict.get(key, 0)
+    the_dict[key] = prev + multiplier
+
+
+def optimized_polymerization(template: str, subs_raw: List[str], steps):
+    substitutions = parse_substitutions(subs_raw)
+    polymer = parse_template(template)
+    for _ in range(0, steps):
+        new_polymer = dict()
+        for pair in polymer:
+            occurrences = polymer[pair]
+            if pair in substitutions:
+                first_pair = pair[0] + substitutions[pair]
+                second_pair = substitutions[pair] + pair[1]
+                increase_by(occurrences, new_polymer, first_pair)
+                increase_by(occurrences, new_polymer, second_pair)
+            else:
+                increase_by(occurrences, new_polymer, pair)
+        polymer = new_polymer
+    character_counter = dict()
+    first_char = template[0]
+    last_char = template[-1]
+    for pair in polymer:
+        first = pair[0]
+        second = pair[1]
+        increase_by(polymer[pair], character_counter, first)
+        increase_by(polymer[pair], character_counter, second)
+    for char in character_counter:
+        if char == first_char or char == last_char:
+            character_counter[char] = int((character_counter[char] + 1) / 2)
+        else:
+            character_counter[char] = int(character_counter[char] / 2)
+    sorted_elements = sorted(character_counter.items(), key=lambda item: item[1])
     return sorted_elements[-1][1] - sorted_elements[0][1]
--- challenges/test_day_14_extended_polymerization.py
+++ challenges/test_day_14_extended_polymerization.py
@@ -1,48 +1,36 @@
 from unittest import TestCase
 
-from challenges.day_14_extended_polymerization import polymer_after_steps, polymer_hash
+from challenges.day_14_extended_polymerization import polymer_hash, polymer_after_steps, parse_substitutions, optimized_polymerization
+
+example_substitutions = [
+    "CH -> B",
+    "HH -> N",
+    "CB -> H",
+    "NH -> C",
+    "HB -> C",
+    "HC -> B",
+    "HN -> C",
+    "NN -> C",
+    "BH -> H",
+    "NC -> B",
+    "NB -> B",
+    "BN -> B",
+    "BB -> N",
+    "BC -> B",
+    "CC -> N",
+    "CN -> C"
+]
 
 
 class Test(TestCase):
+    substitutions = example_substitutions
+
     def test_polymer_after_steps(self):
-        self.assertEqual("NBBNBNBBCCNBCNCCNBBNBBNBBBNBBNBBCBHCBHHNHCBBCBHCB", polymer_after_steps("NNCB", [
-            "CH -> B",
-            "HH -> N",
-            "CB -> H",
-            "NH -> C",
-            "HB -> C",
-            "HC -> B",
-            "HN -> C",
-            "NN -> C",
-            "BH -> H",
-            "NC -> B",
-            "NB -> B",
-            "BN -> B",
-            "BB -> N",
-            "BC -> B",
-            "CC -> N",
-            "CN -> C"
-        ], 4))
+        expected = "NBBNBNBBCCNBCNCCNBBNBBNBBBNBBNBBCBHCBHHNHCBBCBHCB"
+        self.assertEqual(expected, polymer_after_steps("NNCB", parse_substitutions(example_substitutions), 4))
 
     def test_polymer_hash(self):
-        self.assertEqual(1588, polymer_hash("NNCB", [
-            "CH -> B",
-            "HH -> N",
-            "CB -> H",
-            "NH -> C",
-            "HB -> C",
-            "HC -> B",
-            "HN -> C",
-            "NN -> C",
-            "BH -> H",
-            "NC -> B",
-            "NB -> B",
-            "BN -> B",
-            "BB -> N",
-            "BC -> B",
-            "CC -> N",
-            "CN -> C"
-        ], 10))
+        self.assertEqual(1588, polymer_hash("NNCB", example_substitutions, 10))
 
     def test_polymer_hash_real_iunput(self):
         with open('inputs/day_14.txt') as f:
@@ -50,3 +38,17 @@ class Test(TestCase):
             template = all_lines[0]
             substitutions = all_lines[2:]
         self.assertEqual(3408, polymer_hash(template, substitutions, 10))
+
+    def test_small_optimized_polymer_hash(self):
+        self.assertEqual(1, optimized_polymerization("NNCB", example_substitutions, 1))
+        self.assertEqual(1588, optimized_polymerization("NNCB", example_substitutions, 10))
+
+    def test_large_polymer_hash(self):
+        self.assertEqual(2188189693529, optimized_polymerization("NNCB", example_substitutions, 40))
+
+    def test_optimized_polymer_hash_real_iunput(self):
+        with open('inputs/day_14.txt') as f:
+            all_lines = f.read().splitlines()
+            template = all_lines[0]
+            substitutions = all_lines[2:]
+        self.assertEqual(3724343376942, optimized_polymerization(template, substitutions, 40))
