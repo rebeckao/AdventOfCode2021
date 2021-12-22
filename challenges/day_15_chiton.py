638b134ed14d358a6af7c7d9223b82a39d69fb58
rebeckao <rebecka.oefverholm@gmail.com>
2021-12-22 11:42:39 +0100

    Day 15 part 2

--- challenges/day_15_chiton.py
+++ challenges/day_15_chiton.py
@@ -23,3 +23,55 @@ def lowest_risk_breath_first(cave_map: List[str]):
             if lowest_risk_to_location.get(possible_next) is None or lowest_risk_to_location[possible_next] > new_risk:
                 lowest_risk_to_location[possible_next] = new_risk
                 locations_to_explore[possible_next] = new_risk
+
+
+def lowest_risk_large_cave(cave_map: List[str]):
+    lowest_risk_to_location = {(0, 0): 0}
+    locations_to_explore = {(1, 0): int(cave_map[0][1]), (0, 1): int(cave_map[1][0])}
+    original_x_length = len(cave_map[0])
+    original_y_length = len(cave_map)
+    max_x = original_x_length * 5 - 1
+    max_y = original_y_length * 5 - 1
+    large_cave_map = build_large_map(cave_map, original_x_length, original_y_length)
+    goal_risk = int(large_cave_map[max_y][max_x])
+    while len(locations_to_explore) > 0:
+        sorted_elements = sorted(locations_to_explore.items(), key=lambda item: item[1])
+        next_best_location, risk_to_go_there = sorted_elements[0]
+        locations_to_explore.pop(next_best_location)
+        x = next_best_location[0]
+        y = next_best_location[1]
+        if (x == max_x and y == max_y - 1) or (x == max_x - 1 and y == max_y):  # close to goal
+            return risk_to_go_there + goal_risk
+        for possible_next in [(x + 1, y), (x, y + 1), (x - 1, y), (x, y - 1)]:
+            new_x = possible_next[0]
+            new_y = possible_next[1]
+            if new_x < 0 or new_x > max_x or new_y < 0 or new_y > max_y:
+                continue
+            new_risk = risk_to_go_there + int(large_cave_map[new_y][new_x])
+            if lowest_risk_to_location.get(possible_next) is None or lowest_risk_to_location[possible_next] > new_risk:
+                lowest_risk_to_location[possible_next] = new_risk
+                locations_to_explore[possible_next] = new_risk
+
+
+def build_large_map(cave_map, original_x_length, original_y_length):
+    large_cave_map = []
+    for y_round in range(0, 5):
+        for y_in_map in range(0, original_y_length):
+            row = y_round * original_y_length + y_in_map
+            large_cave_map.append("")
+            for x_round in range(0, 5):
+                for x_in_map in range(0, original_x_length):
+                    col = x_round * original_x_length + x_in_map
+                    large_cave_map[row] += str(risk_at_location(cave_map, col, row, original_x_length, original_y_length))
+    return large_cave_map
+
+
+def risk_at_location(cave_map, x, y, x_length, y_length):
+    x_increase = int(x / x_length)
+    y_increase = int(y / y_length)
+    x_pos_in_map = x % x_length
+    y_pos_in_map = y % y_length
+    value_in_original_map = int(cave_map[y_pos_in_map][x_pos_in_map])
+    increased_value = value_in_original_map + x_increase + y_increase
+    modulated_value = ((increased_value - 1) % 9) + 1
+    return modulated_value
--- challenges/test_day_15_chiton.py
+++ challenges/test_day_15_chiton.py
@@ -1,6 +1,6 @@
 from unittest import TestCase
 
-from challenges.day_15_chiton import lowest_risk_breath_first
+from challenges.day_15_chiton import lowest_risk_breath_first, risk_at_location, lowest_risk_large_cave
 
 
 class Test(TestCase):
@@ -54,4 +54,40 @@ class Test(TestCase):
     def test_lowest_risk_real_input(self):
         with open('inputs/day_15.txt') as f:
             all_lines = f.read().splitlines()
-            self.assertEqual(19, lowest_risk_breath_first(all_lines))
+            self.assertEqual(589, lowest_risk_breath_first(all_lines))
+
+    def test_risk_at_location(self):
+        self.assertEqual(8, risk_at_location(["8"], 0, 0, 1, 1))
+        self.assertEqual(1, risk_at_location(["8"], 1, 1, 1, 1))
+        self.assertEqual(5, risk_at_location(["8"], 2, 4, 1, 1))
+        self.assertEqual(9, risk_at_location([
+            "1163751742",
+            "1381373672",
+            "2136511328",
+            "3694931569",
+            "7463417111",
+            "1319128137",
+            "1359912421",
+            "3125421639",
+            "1293138521",
+            "2311944581"
+        ], 49, 49, 10, 10))
+
+    def test_lowest_risk_large_cave(self):
+        self.assertEqual(315, lowest_risk_large_cave([
+            "1163751742",
+            "1381373672",
+            "2136511328",
+            "3694931569",
+            "7463417111",
+            "1319128137",
+            "1359912421",
+            "3125421639",
+            "1293138521",
+            "2311944581"
+        ]))
+
+    def test_lowest_risk_large_cave_real_input(self):
+        with open('inputs/day_15.txt') as f:
+            all_lines = f.read().splitlines()
+            self.assertEqual(2885, lowest_risk_large_cave(all_lines))
