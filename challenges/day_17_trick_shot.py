ecf521e78b961944dbc277a336356d08347ced4c
rebeckao <rebecka.oefverholm@gmail.com>
2021-12-23 18:14:16 +0100

    Day 17 part 2

--- challenges/day_17_trick_shot.py
+++ challenges/day_17_trick_shot.py
@@ -4,4 +4,38 @@ def highest_y(goal_min_y):
     # overshooting the target area on the way down, which means that it goes from 0 to
     # the minimum target y in just one step.
     initial_y_velocity = -goal_min_y
+    return highest_point_reached_by_y_velocity(initial_y_velocity)
+
+
+def highest_point_reached_by_y_velocity(initial_y_velocity):
     return initial_y_velocity * (initial_y_velocity - 1) / 2
+
+
+def reaches_goal(goal_min_x, goal_max_x, goal_min_y, goal_max_y, original_x_velocity, original_y_velocity) -> bool:
+    current_x_velocity = original_x_velocity
+    current_y_velocity = original_y_velocity
+    current_x = 0
+    current_y = 0
+    while current_x <= goal_max_x and current_y >= goal_min_y:
+        if current_x >= goal_min_x and current_y <= goal_max_y:
+            return True
+        current_x += current_x_velocity
+        current_y += current_y_velocity
+        current_x_velocity = max(current_x_velocity - 1, 0)
+        current_y_velocity -= 1
+    return False
+
+
+def number_of_different_velocities_landing_in_target(goal_min_x, goal_max_x, goal_min_y, goal_max_y):
+    highest_possible_y = -goal_min_y
+    lowest_possible_y = goal_min_y  # reaches the lowest part of goal after 1 step
+    lowest_possible_x = 6  # 6 for example, 17 for the real input
+    highest_possible_x = goal_max_x
+    number_of_possible_velocities = 0
+    possible_velocities = set()
+    for x in range(lowest_possible_x, highest_possible_x + 1):
+        for y in range(lowest_possible_y, highest_possible_y + 1):
+            if reaches_goal(goal_min_x, goal_max_x, goal_min_y, goal_max_y, x, y):
+                number_of_possible_velocities += 1
+                possible_velocities.add((x, y))
+    return number_of_possible_velocities
--- challenges/test_day_17_trick_shot.py
+++ challenges/test_day_17_trick_shot.py
@@ -1,6 +1,6 @@
 from unittest import TestCase
 
-from challenges.day_17_trick_shot import highest_y
+from challenges.day_17_trick_shot import highest_y, number_of_different_velocities_landing_in_target, reaches_goal
 
 
 class Test(TestCase):
@@ -9,3 +9,16 @@ class Test(TestCase):
 
     def test_highest_y_real_input(self):
         self.assertEqual(6441, highest_y(-114))
+
+    def test_number_of_different_velocities_landing_in_target(self):
+        self.assertEqual(112, number_of_different_velocities_landing_in_target(20, 30, -10, -5))
+
+    def test_number_of_different_velocities_landing_in_target_real_input(self):
+        self.assertEqual(3186, number_of_different_velocities_landing_in_target(153, 199, -114, -75))
+
+    def test_reaches_goal(self):
+        self.assertEqual(True, reaches_goal(20, 30, -10, -5, 26, -10))
+        self.assertEqual(True, reaches_goal(20, 30, -10, -5, 10, -2))
+        self.assertEqual(True, reaches_goal(20, 30, -10, -5, 6, 6))
+        self.assertEqual(False, reaches_goal(20, 30, -10, -5, 6, -10))
+        self.assertEqual(False, reaches_goal(20, 30, -10, -5, 10, 6))
