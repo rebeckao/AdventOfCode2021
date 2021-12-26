a860c9595b5755ee5f278689df514c124ca3a057
rebeckao <rebecka.oefverholm@gmail.com>
2021-12-26 12:17:10 +0100

    Day 18 part 1

---
+++ challenges/day_18_snailfish_strings.py
@@ -0,0 +1,120 @@
+from dataclasses import dataclass
+from math import floor, ceil
+from typing import List, Union, Tuple
+
+
+def add(number_1: str, number_2: str) -> str:
+    return f"[{number_1},{number_2}]"
+
+
+def add_explosion_result(number: str, value, coming_from_side) -> str:
+    if coming_from_side == 1:
+        for idx, char in enumerate(number):
+            if char.isdigit():
+                steps_to_end_of_number = end_of_number(number[idx:])
+                new_value = (int(number[idx:idx + steps_to_end_of_number]) + value)
+                return number[0:idx] + str(new_value) + number[idx + steps_to_end_of_number:]
+    else:
+        for idx in range(len(number) - 1, -1, -1):
+            if number[idx].isdigit():
+                steps_to_start_of_number = start_of_number(number[0:idx])
+                new_value = (int(number[steps_to_start_of_number: idx + 1]) + value)
+                return number[0:steps_to_start_of_number] + str(new_value) + number[idx + 1:]
+    return number
+
+
+def end_of_number(number):
+    for idx in range(0, len(number)):
+        if not number[idx].isdigit():
+            return idx
+
+
+def start_of_number(number: str) -> int:
+    for idx in range(len(number) - 1, -1, -1):
+        if not number[idx].isdigit():
+            return idx + 1
+
+
+def explode(number: str) -> str:
+    level = 0
+    for idx, char in enumerate(number):
+        if char == "[":
+            level += 1
+        elif char == "]":
+            level -= 1
+        elif char.isdigit() and level > 4:
+            steps_to_comma = number[idx:].index(",")
+            steps_to_end_pair = number[idx:].index("]")
+            left_val = int(number[idx:idx + steps_to_comma])
+            right_val = int(number[idx + steps_to_comma + 1:idx + steps_to_end_pair])
+            left_side = add_explosion_result(number[0:idx - 1], left_val, 0)
+            right_side = add_explosion_result(number[idx + steps_to_end_pair + 1:], right_val, 1)
+            return f"{left_side}0{right_side}"
+    return number
+
+
+def split(number: str) -> str:
+    for idx in range(0, len(number) - 1):
+        if number[idx].isdigit() and number[idx + 1].isdigit():
+            val = int(number[idx:idx + 2])
+            left_number = floor(val / 2.0)
+            right_number = ceil(val / 2.0)
+            return f"{number[0:idx]}[{left_number},{right_number}]{number[idx + 2:]}"
+    return number
+
+
+def reduce_one_step(number: str) -> str:
+    after_explode = explode(number)
+    if after_explode != number:
+        return after_explode
+    after_split = split(number)
+    return after_split
+
+
+def reduce(number: str) -> str:
+    before_reduction = number
+    while True:
+        after_reduction = reduce_one_step(before_reduction)
+        if after_reduction == before_reduction:
+            return after_reduction
+        before_reduction = after_reduction
+
+
+def add_together_list(raw_numbers: List[str]) -> str:
+    result = raw_numbers[0]
+    for raw_number in raw_numbers[1:]:
+        result = add(result, raw_number)
+        result = reduce(result)
+    return result
+
+
+@dataclass
+class SnailfishNumber:
+    plain_value: Union[int, None] = None
+    pair: Union[Tuple, None] = None
+
+
+def parse_snailfish_expression(raw_number: str) -> SnailfishNumber:
+    number, _ = parse_snailfish_number(raw_number)
+    return number
+
+
+def parse_snailfish_number(raw_number: str) -> Tuple[SnailfishNumber, str]:
+    next_char = raw_number[0]
+    if next_char == "[":  # Pair
+        left_number, rest_of_string = parse_snailfish_number(raw_number[1:])
+        if rest_of_string[0] != ",":
+            raise Exception("rest of string should start with ,: " + rest_of_string)
+        right_number, rest_of_string = parse_snailfish_number(rest_of_string[1:])
+        if rest_of_string[0] != "]":
+            raise Exception("rest of string should start with ]: " + rest_of_string)
+        return SnailfishNumber(None, (left_number, right_number)), rest_of_string[1:]
+    elif next_char.isdigit():  # Regular number
+        return SnailfishNumber(int(next_char), None), raw_number[1:]
+
+
+def magnitude(number: SnailfishNumber) -> int:
+    if number.plain_value is not None:
+        return number.plain_value
+    else:
+        return 3 * magnitude(number.pair[0]) + 2 * magnitude(number.pair[1])
---
+++ challenges/test_day_18_snailfish_strings.py
@@ -0,0 +1,99 @@
+from unittest import TestCase
+
+from challenges.day_18_snailfish_strings import add_together_list, reduce_one_step, reduce, magnitude, parse_snailfish_expression
+
+
+class Test(TestCase):
+    def test_magnitude(self):
+        self.assertEqual(29, magnitude(parse_snailfish_expression("[9,1]")))
+        self.assertEqual(129, magnitude(parse_snailfish_expression("[[9,1],[1,9]]")))
+        self.assertEqual(1384, magnitude(parse_snailfish_expression("[[[[0,7],4],[[7,8],[6,0]]],[8,1]]")))
+        self.assertEqual(445, magnitude(parse_snailfish_expression("[[[[1,1],[2,2]],[3,3]],[4,4]]")))
+        self.assertEqual(1137, magnitude(parse_snailfish_expression("[[[[5,0],[7,4]],[5,5]],[6,6]]")))
+        self.assertEqual(3488, magnitude(parse_snailfish_expression("[[[[8,7],[7,7]],[[8,6],[7,7]]],[[[0,7],[6,6]],[8,7]]]")))
+
+    def test_add_together_list(self):
+        self.assertEqual("[[1,2],[[3,4],5]]", add_together_list([
+            "[1,2]",
+            "[[3,4],5]"
+        ]))
+        self.assertEqual("[[[[0,7],4],[[7,8],[6,0]]],[8,1]]", add_together_list([
+            "[[[[4,3],4],4],[7,[[8,4],9]]]",
+            "[1,1]"
+        ]))
+        self.assertEqual("[[[[1,1],[2,2]],[3,3]],[4,4]]", add_together_list([
+            "[1,1]",
+            "[2,2]",
+            "[3,3]",
+            "[4,4]"
+        ]))
+        self.assertEqual("[[[[3,0],[5,3]],[4,4]],[5,5]]", add_together_list([
+            "[1,1]",
+            "[2,2]",
+            "[3,3]",
+            "[4,4]",
+            "[5,5]"
+        ]))
+        self.assertEqual("[[[[5,0],[7,4]],[5,5]],[6,6]]", add_together_list([
+            "[1,1]",
+            "[2,2]",
+            "[3,3]",
+            "[4,4]",
+            "[5,5]",
+            "[6,6]"
+        ]))
+        self.assertEqual("[[[[4,0],[5,4]],[[7,7],[6,0]]],[[8,[7,7]],[[7,9],[5,0]]]]", add_together_list([
+            "[[[0,[4,5]],[0,0]],[[[4,5],[2,6]],[9,5]]]",
+            "[7,[[[3,7],[4,3]],[[6,3],[8,8]]]]"
+        ]))
+        self.assertEqual("[[[[8,7],[7,7]],[[8,6],[7,7]]],[[[0,7],[6,6]],[8,7]]]", add_together_list([
+            "[[[0,[4,5]],[0,0]],[[[4,5],[2,6]],[9,5]]]",
+            "[7,[[[3,7],[4,3]],[[6,3],[8,8]]]]",
+            "[[2,[[0,8],[3,4]]],[[[6,7],1],[7,[1,6]]]]",
+            "[[[[2,4],7],[6,[0,5]]],[[[6,8],[2,8]],[[2,1],[4,5]]]]",
+            "[7,[5,[[3,8],[1,4]]]]",
+            "[[2,[2,2]],[8,[8,1]]]",
+            "[2,9]",
+            "[1,[[[9,3],9],[[9,0],[0,7]]]]",
+            "[[[5,[7,4]],7],1]",
+            "[[[[4,2],2],6],[8,7]]"
+        ]))
+        self.assertEqual("[[[[6,6],[7,6]],[[7,7],[7,0]]],[[[7,7],[7,7]],[[7,8],[9,9]]]]", add_together_list([
+            "[[[0,[5,8]],[[1,7],[9,6]]],[[4,[1,2]],[[1,4],2]]]",
+            "[[[5,[2,8]],4],[5,[[9,9],0]]]",
+            "[6,[[[6,2],[5,6]],[[7,6],[4,7]]]]",
+            "[[[6,[0,7]],[0,9]],[4,[9,[9,0]]]]",
+            "[[[7,[6,4]],[3,[1,3]]],[[[5,5],1],9]]",
+            "[[6,[[7,3],[3,2]]],[[[3,8],[5,7]],4]]",
+            "[[[[5,4],[7,7]],8],[[8,3],8]]",
+            "[[9,3],[[9,9],[6,[4,9]]]]",
+            "[[2,[[7,7],7]],[[5,8],[[9,3],[0,2]]]]",
+            "[[[[5,2],5],[8,[3,7]]],[[5,[7,5]],[4,4]]]"
+        ]))
+
+    def test_single_reduce_step(self):
+        self.assertEqual("[[[[0,9],2],3],4]", reduce_one_step("[[[[[9,8],1],2],3],4]"))
+        self.assertEqual("[[[[0,7],4],[7,[[8,4],9]]],[1,1]]", reduce_one_step("[[[[[4,3],4],4],[7,[[8,4],9]]],[1,1]]"))
+        self.assertEqual("[7,[6,[5,[7,0]]]]", reduce_one_step("[7,[6,[5,[4,[3,2]]]]]"))
+        self.assertEqual("[[6,[5,[7,0]]],3]", reduce_one_step("[[6,[5,[4,[3,2]]]],1]"))
+        self.assertEqual("[[3,[2,[8,0]]],[9,[5,[4,[3,2]]]]]", reduce_one_step("[[3,[2,[1,[7,3]]]],[6,[5,[4,[3,2]]]]]"))
+        self.assertEqual("[[3,[2,[8,0]]],[9,[5,[7,0]]]]", reduce_one_step("[[3,[2,[8,0]]],[9,[5,[4,[3,2]]]]]"))
+        self.assertEqual("[[[[0,7],4],[15,[0,13]]],[1,1]]", reduce_one_step("[[[[0,7],4],[7,[[8,4],9]]],[1,1]]"))
+        self.assertEqual("[[[0,[5,8]],[[1,7],[9,6]]],[[4,[1,2]],[[1,4],2]]]", reduce_one_step("[[[0,[5,8]],[[1,7],[9,6]]],[[4,[1,2]],[[1,4],2]]]"))
+        self.assertEqual("[[[5,[2,8]],4],[5,[[9,9],0]]]", reduce_one_step("[[[5,[2,8]],4],[5,[[9,9],0]]]"))
+        self.assertEqual("[6,[[[6,2],[5,6]],[[7,6],[4,7]]]]", reduce_one_step("[6,[[[6,2],[5,6]],[[7,6],[4,7]]]]"))
+        self.assertEqual("[[[6,[0,7]],[0,9]],[4,[9,[9,0]]]]", reduce_one_step("[[[6,[0,7]],[0,9]],[4,[9,[9,0]]]]"))
+        self.assertEqual("[[[7,[6,4]],[3,[1,3]]],[[[5,5],1],9]]", reduce_one_step("[[[7,[6,4]],[3,[1,3]]],[[[5,5],1],9]]"))
+        self.assertEqual("[[6,[[7,3],[3,2]]],[[[3,8],[5,7]],4]]", reduce_one_step("[[6,[[7,3],[3,2]]],[[[3,8],[5,7]],4]]"))
+        self.assertEqual("[[[[5,4],[7,7]],8],[[8,3],8]]", reduce_one_step("[[[[5,4],[7,7]],8],[[8,3],8]]"))
+        self.assertEqual("[[9,3],[[9,9],[6,[4,9]]]]", reduce_one_step("[[9,3],[[9,9],[6,[4,9]]]]"))
+        self.assertEqual("[[2,[[7,7],7]],[[5,8],[[9,3],[0,2]]]]", reduce_one_step("[[2,[[7,7],7]],[[5,8],[[9,3],[0,2]]]]"))
+        self.assertEqual("[[[5,2],5],[8,[3,7]]],[[5,[7,5]],[4,4]]]", reduce_one_step("[[[5,2],5],[8,[3,7]]],[[5,[7,5]],[4,4]]]"))
+
+    def test_reduce(self):
+        self.assertEqual("[[[[6,6],[7,6]],[[7,7],[7,0]]],[[[7,7],[7,7]],[[7,8],[9,9]]]]", reduce("[[[[[7,7],[7,7]],[[7,7],[7,7]]],[[[8,7],[8,7]],[[7,9],[5,0]]]],[[[[5,2],5],[8,[3,7]]],[[5,[7,5]],[4,4]]]]"))
+
+    def test_add_together_list_real_input(self):
+        with open('inputs/day_18.txt') as f:
+            lines = f.read().splitlines()
+            self.assertEqual(4243, magnitude(parse_snailfish_expression(add_together_list(lines))))
