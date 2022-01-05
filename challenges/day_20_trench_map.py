732b1e62752c2c1663c8576b7fe3d4b5a201f85f
rebeckao <rebecka.oefverholm@gmail.com>
2022-01-05 20:33:11 +0100

    Day 20 part 2

--- challenges/day_20_trench_map.py
+++ challenges/day_20_trench_map.py
@@ -1,9 +1,9 @@
 from typing import List
 
 
-def lit_pixels_after_two_passes(image_enhancement_algorithm: str, image: List[str]):
+def lit_pixels_after_passes(image_enhancement_algorithm: str, image: List[str], passes: int):
     margin = 10
-    for step in range(0, 2):
+    for step in range(0, passes):
         width = len(image[0])
         empty_line = "".ljust(width + margin * 2, ".")
         enlarged_image = [empty_line for _ in range(0, margin)]
@@ -19,13 +19,13 @@ def lit_pixels_after_two_passes(image_enhancement_algorithm: str, image: List[st
                 decimal = _binary_to_decimal(number.replace("#", "1").replace(".", "0"))
                 new_pixel = image_enhancement_algorithm[decimal]
                 new_image[row_idx] = new_image[row_idx][0: col_idx] + str(new_pixel) + new_image[row_idx][col_idx + 1:]
-        for line in new_image:
-            print(line)
         image = new_image
-    pruned_image = [line[margin + 4:-margin - 4] for line in image[margin + 4:-margin + 4]]
-    for line in pruned_image:
+        if step % 2 == 1:
+            pruned_image = [line[margin * 2 - 2:-(margin * 2 - 2)] for line in image[margin * 2 - 2:-margin + 2]]
+            image = pruned_image
+    for line in image:
         print(line)
-    return sum([line.count("#") for line in pruned_image])
+    return sum([line.count("#") for line in image])
 
 
 def _binary_to_decimal(binary: str) -> int:
--- challenges/test_day_20_trench_map.py
+++ challenges/test_day_20_trench_map.py
@@ -1,6 +1,6 @@
 from unittest import TestCase
 
-from challenges.day_20_trench_map import lit_pixels_after_two_passes
+from challenges.day_20_trench_map import lit_pixels_after_passes
 
 
 class Test(TestCase):
@@ -19,9 +19,31 @@ class Test(TestCase):
             "..#..",
             "..###"
         ]
-        self.assertEqual(35, lit_pixels_after_two_passes(iea, input_image))
+        self.assertEqual(35, lit_pixels_after_passes(iea, input_image, 2))
 
     def test_lit_pixels_after_two_passes_real_input(self):
         with open('inputs/day_20.txt') as f:
             lines = f.read().splitlines()
-            self.assertEqual(4873, lit_pixels_after_two_passes(lines[0], lines[2:]))
+            self.assertEqual(4873, lit_pixels_after_passes(lines[0], lines[2:], 2))
+
+    def test_lit_pixels_after_fifty_passes(self):
+        iea = "..#.#..#####.#.#.#.###.##.....###.##.#..###.####..#####..#....#..#..##..##" \
+              + "#..######.###...####..#..#####..##..#.#####...##.#.#..#.##..#.#......#.###" \
+              + ".######.###.####...#.##.##..#..#..#####.....#.#....###..#.##......#.....#." \
+              + ".#..#..##..#...##.######.####.####.#.#...#.......#..#.#.#...####.##.#....." \
+              + ".#..#...##.#.##..#...##.#.##..###.#......#.#.......#.#.#.####.###.##...#.." \
+              + "...####.#..#..#.##.#....##..#.####....##...##..#...#......#.#.......#....." \
+              + "..##..####..#...#.#.#...##..#.#..###..#####........#..####......#..#"
+        input_image = [
+            "#..#.",
+            "#....",
+            "##..#",
+            "..#..",
+            "..###"
+        ]
+        self.assertEqual(3351, lit_pixels_after_passes(iea, input_image, 50))
+
+    def test_lit_pixels_after_fifty_passes_real_input(self):
+        with open('inputs/day_20.txt') as f:
+            lines = f.read().splitlines()
+            self.assertEqual(16394, lit_pixels_after_passes(lines[0], lines[2:], 50))
